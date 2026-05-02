0369197274a105d90b6efbdcfbe0c193d8813905
Arturo Lopez <jlopezm8@upao.edu.pe>
2026-05-02 11:00:01 -0500

    feat: Implementación de las clases "prueba" Pato y pavo respectivamente

---
+++ src/Domain/PatoReal.java
@@ -0,0 +1,15 @@
+package Domain;
+
+public class PatoReal implements Pato{
+
+    @Override
+    public void cuaquear() {
+        System.out.println("Cuac Cuac");
+          }
+
+    @Override
+    public void volar() {
+        System.out.println("Estoy volando");
+        }
+    
+}
---
+++ src/Domain/PavoSilvestre.java
@@ -0,0 +1,15 @@
+package Domain;
+
+public class PavoSilvestre implements Pavo{
+
+    @Override
+    public void gluglutear() {
+        System.out.println("Gluglu gluglu");
+    }
+
+    @Override
+    public void volar() {
+        System.out.println("Estoy volando una distancia corta");
+            }
+    
+}
